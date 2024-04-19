e9a5556d4479f9f5260381c17b0e43b7ff456731
Clare <72578735+clarechan1738@users.noreply.github.com>
2024-04-18 23:37:00 -0400

    added cool colours
    woooooooooooooooooooooooooooooooooooooooooooooooo

--- Assets/ColourSwap.cs
+++ Assets/ColourSwap.cs
@@ -7,6 +7,10 @@ public class ColourSwap : MonoBehaviour
     [SerializeField] Material ColourBLue;
     [SerializeField] Material ColourGreen;
     [SerializeField] Material ColourRede;
+    [SerializeField] Material ColourPurpel;
+    [SerializeField] Material ColourPINK;
+    [SerializeField] Material ColourYHellow;
+    [SerializeField] Material ColourOrang;
     [SerializeField] Material ColourBasic;
 
     private int current = 0;
@@ -29,18 +33,34 @@ public class ColourSwap : MonoBehaviour
         switch(current)
         {
             case 0:
-                this.GetComponent<Renderer>().material = ColourBLue;
-                current++;
-                break;
-            case 1:
-                this.GetComponent<Renderer>().material = ColourGreen;
-                current++;
-                break;
-            case 2:
                 this.GetComponent<Renderer>().material = ColourRede;
                 current++;
                 break;
+            case 1:
+                this.GetComponent<Renderer>().material = ColourOrang;
+                current++;
+                break;
+            case 2:
+                this.GetComponent<Renderer>().material = ColourYHellow;
+                current++;
+                break;
             case 3:
+                this.GetComponent<Renderer>().material = ColourGreen;
+                current++;
+                break;
+            case 4:
+                this.GetComponent<Renderer>().material = ColourBLue;
+                current++;
+                break;
+            case 5:
+                this.GetComponent<Renderer>().material = ColourPurpel;
+                current++;
+                break;
+            case 6:
+                this.GetComponent<Renderer>().material = ColourPINK;
+                current++;
+                break;
+            case 7:
                 this.GetComponent<Renderer>().material = ColourBasic;
                 current = 0;
                 break;
